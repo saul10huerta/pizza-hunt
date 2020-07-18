fa16c03a29c6e2e8bfc81112ff27b6f646b67607
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-07-18 16:40:47 -0500

    added validation

--- models/Comment.js
+++ models/Comment.js
@@ -10,10 +10,14 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: 'You need to provide your name!',
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -31,10 +35,14 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: 'You need to provide your name!',
+      trim: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
